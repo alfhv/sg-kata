9adc6df775e01f6606a819488f3065851619f66d
ahv <alfredohidalgovega@gmail.com>
2017-01-06 22:34:36 +0100

    first commit

---
+++ KataSG.Tests/AccountTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace KataSG.Tests
+{
+    [TestFixture]
+    public class AccountTests
+    {
+        [Test]
+        public void Should_Proecss_Deposit()
+        {
+            var account = new Account();
+
+            var deposits = new Random();
+            var deposit1 = deposits.Next(0, 100);
+            account.Deposit(deposit1);
+
+            Assert.AreEqual(deposit1, account.GetBalance());
+        }
+
+        [Test]
+        public void Should_Process_Deposits()
+        {
+            var account = new Account();
+
+            var deposits = new Random();
+
+            var deposit1 = deposits.Next(0, 100);
+            account.Deposit(deposit1);
+
+            var deposit2 = deposits.Next(0, 100);
+            account.Deposit(deposit2);
+
+            var deposit3 = deposits.Next(0, 100);
+            account.Deposit(deposit3);
+
+            Assert.AreEqual(deposit1 + deposit2 + deposit3, account.GetBalance());
+        }
+
+        [Test]
+        public void Should_Process_Withdrawal()
+        {
+            var account = new Account();
+
+            var deposits = new Random();
+
+            var deposit1 = deposits.Next(0, 100);
+            account.Deposit(deposit1);
+
+            // second deposit to ensure balance is not negative
+            var deposit2 = deposits.Next(0, 100);
+            account.Deposit(deposit2);
+
+            var withdrawal = deposits.Next(0, 100);
+            account.Withdrawal(withdrawal);
+
+            Assert.AreEqual(deposit1 + deposit2 - withdrawal, account.GetBalance());
+        }
+
+        [Test]
+        public void Should_Transfer()
+        {
+            var accountSource = new Account();
+            var accountTarget = new Account();
+
+            var deposits = new Random();
+            var deposit1 = deposits.Next(0, 100);
+            accountSource.Deposit(deposit1);
+
+            var deposit2 = deposits.Next(0, 100);
+            accountSource.Deposit(deposit2);
+
+            var transferAmount = deposits.Next(0, 100);
+
+            accountSource.TransferTo(accountTarget, transferAmount);
+
+            Assert.AreEqual(deposit1 + deposit2 - transferAmount, accountSource.GetBalance());
+            Assert.AreEqual(transferAmount, accountTarget.GetBalance());
+        }
+    }
+}
---
+++ KataSG/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataSG
+{
+    public class Account
+    {
+        private List<double> _transactions;
+
+        public Account()
+        {
+            _transactions = new List<double>();
+        }
+
+        /// <summary>
+        /// Accept positive amounts only, if negative received, it will be converted to positive
+        /// Error could be raised but it is not in the scope of this kata
+        /// </summary>
+        /// <param name="amount">P</param>
+        public void Deposit(double amount)
+        {
+            _transactions.Add(Math.Abs(amount));
+        }
+
+        /// <summary>
+        /// Very simple implementation, enough for now
+        /// </summary>
+        /// <returns></returns>
+        public double GetBalance()
+        {
+            return _transactions.Sum();
+        }
+
+        public void TransferTo(Account accountTarget, int transferAmount)
+        {
+            Withdrawal(transferAmount);
+            accountTarget.Deposit(transferAmount);
+        }
+
+        /// <summary>
+        /// Only positive amount also, as in Deposit(), we take care of negative convertion inside
+        /// </summary>
+        /// <param name="withdrawal"></param>
+        public void Withdrawal(int withdrawal)
+        {
+            _transactions.Add(Math.Abs(withdrawal) * -1);
+        }
+    }
+}
